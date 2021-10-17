948fa751e866b488b29217fe58dc8595573e14e1
Ayelen Lafonte <ayelenlafonte@gmail.com>
2021-10-17 19:30:23 +0200

    Creo Search

---
+++ js/controllers/LoaderController.js
@@ -0,0 +1,26 @@
+import PubSub from "../services/PubSub.js"
+import { loaderView } from "../views.js"
+
+export default class LoaderController {
+
+    constructor(element) {
+        this.element = element
+        this.element.innerHTML = loaderView()  // Obtiene la estructura html del loader
+
+        // Suscribe a los eventos y actúa segun corresponda
+        PubSub.subscribe(PubSub.events.SHOW_LOADING, () => {
+            this.showLoader()
+        })
+        PubSub.subscribe(PubSub.events.HIDE_LOADING, () => {
+            this.hideLoader()
+        })        
+    }
+
+    hideLoader() {
+        this.element.style.display = "none" 
+    }
+
+    showLoader() {
+        this.element.style.display = "initial" 
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
+import MessageController from './controllers/MessageController.js'
+import LoaderController from './controllers/LoaderController.js'
+import AdsListController from './controllers/AdsListController.js'
+import SearchController from './controllers/SearchController.js'
+
+
+
 window.addEventListener('DOMContentLoaded', function() {
 
     // Controlador de mensaje de error
-    const errorDiv = document.querySelector('.error-message')
-    const errorMessageController = new ErrorMessageController(errorDiv)
+    const messages = document.querySelector('.message')
+    new MessageController(messages)
 
-    // coger el elemento del DOM (HTML) donde quiero cargar los tweets
-    const tweetListDiv = document.querySelector('.tweet-list')
+    // Controlador del loader
+    const loaderDiv = document.querySelector('.loader')
+    new LoaderController(loaderDiv)
 
-    // crear un controlador pasándole el elemento del DOM donde cargar los tweets
-    const tweetListController = new TweetListController(tweetListDiv, errorMessageController)
+    // Controlador de la lista de anuncios
+    const adsListDiv = document.querySelector('.ads-list') // Selecciona el elemento del DOM (HTML) donde quiero cargar los anuncios
+    const adsListController = new AdsListController(tweetListDiv, errorMessageController) // Instancia controlador pasándole el elemento del DOM donde cargar los anuncios
+    adsListController.renderAds() // Renderiza los anuncios
 
-    // decir al controlador que pinte los tweets
-    tweetListController.renderTweets()
-
+    // Controlador del buscador
     const search = document.querySelector('#search')
     new SearchController(search)
 
